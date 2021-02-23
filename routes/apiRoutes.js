b9fa1a39cdf45623461d6b46f38520aa48c70501
Maryam Chaudhry <maryamc1798@gmail.com>
2021-02-23 12:17:50 -0500

    reworking api routes to use router instead of function app

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,33 @@
 // const fs = require("fs");
-const notesData = require("../Develop/db/db.json");
+// const notesData = require("../Develop/db/db.json");
 
-module.export = function(app){
-    app.get("/api/notes", function (req,res){
-        res.json(notesData);
-    });
+const router = require("express").Router();
+const dbdata = require("../db/dbdata");
 
-    app.post("/api/notes", function(req,res){
-        notesData.push(req.body);
-    })
-}
+// module.export = function(app){
+//     app.get("/api/notes", function (req,res){
+//         res.json(notesData);
+//     });
+
+//     app.post("/api/notes", function(req,res){
+//         notesData.push(req.body);
+//     })
+
+
+// creating CRUD request for api
+// getting
+router.get("/notes", (req, res) => {
+    dbdata.getNotes().then((notes) => res.json(notes));
+});
+
+// adding
+router.post("/notes", (req,res) => {
+    dbdata.add(req.body).then((note) => res.json(note));
+});
+
+//deleting
+router.delete("/notes/:id", (req, res) => {
+    dbdata.destroy(req.params.id).then(() => res.json({ok: true}));
+});
+
+module.exports = router;
